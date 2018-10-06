5545b45e12d7bbabe42e95639ffe7219fe15e2ae
Antony_Air <antony1380@gmail.com>
2018-10-06 15:35:33 +0900

    init project

---
+++ StepKit/StepKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepKit
+//
+//  Created by Antony on 2018/10/6.
+//  Copyright © 2018 CONTI Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
