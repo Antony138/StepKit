c9f2222c8bf0a37d0d85cf7eb2268eb6126fe1d4
Antony_Air <antony1380@gmail.com>
2018-10-07 20:52:38 +0900

    #2 Get Step from HealthKit (Use HKStatisticsCollectionQuery)

--- StepKit/StepKit/ViewController.swift
+++ StepKit/StepKit/ViewController.swift
@@ -31,6 +31,11 @@ class ViewController: UIViewController {
             print("HealthKit Successfully Authorized.")
         }
     }
+    
+    @IBAction func readSteps(_ sender: UIButton) {
+        readSteps(months: 1, intervalDays: 1) { (success, stepsCollection, error) in
+        }
+    }
 }
 
 
@@ -41,7 +46,12 @@ enum HealthkitSetupError: Error {
 }
 
 extension ViewController {
-    // Authorizing HealthKit
+    /// Authorizing HealthKit
+    ///
+    /// Use to Request authorization from HealthKit.
+    ///
+    /// - Parameters:
+    ///   - completion: The callback.
     func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
         
         // 1. Check to see if HealthKit Is Available on this device
@@ -68,10 +78,66 @@ extension ViewController {
             completion(success, error)
         }
     }
+    
+    /// readSteps Method
+    ///
+    /// Use to Read Steps from HealthKit.
+    ///
+    /// - Parameters:
+    ///   - months: How many months do you want to read.
+    ///   - intervalDays: The fixed-length time intervals.
+    ///   - completion: The callback.
+    ///   - success: The result status of the callback.
+    ///   - stepsCollection: Include the steps data (Use enumerateStatistics: method to parsing data).
+    ///   - error: Return error if something wrong.
+    func readSteps(months: Int, intervalDays: Int, completion: @escaping (_ success: Bool, _ stepsCollection: HKStatisticsCollection, _ error: Error?) -> Swift.Void) {
+        let calendar = NSCalendar.current
+        let now = Date()
+        let startOfToday = NSCalendar.current.startOfDay(for: now)
+        
+        // QuantityType
+        guard let quantityType = HKObjectType.quantityType(forIdentifier: .stepCount) else {
+            fatalError("*** Unable to create a step count type ***")
+        }
+        
+        // Predicate
+        guard let startDate = calendar.date(byAdding: .month, value: -months, to: startOfToday, wrappingComponents: false)  else {
+            fatalError("*** Unable to calculate the start date ***")
+        }
+        let quantitySmaplePredicate = HKQuery.predicateForSamples(withStart: startDate, end: now, options: HKQueryOptions())
+        
+        // Anchor Date
+        // The anchor’s exact date doesn’t matter. So I made it as the start of “Today”
+        let anchorDate = startOfToday
+
+        // intervalComponent:
+        let intervalComponent = NSDateComponents()
+        intervalComponent.day = intervalDays
+
+        // Create the query
+        let collectionQuery = HKStatisticsCollectionQuery(quantityType: quantityType,
+                                                          quantitySamplePredicate: quantitySmaplePredicate,
+                                                          options: .cumulativeSum,
+                                                          anchorDate: anchorDate,
+                                                          intervalComponents: intervalComponent as DateComponents)
+        
+        // Set the results handler
+        collectionQuery.initialResultsHandler = { query, results, error in
+            guard let stepsCollection = results else {
+                fatalError("*** An error occurred while calculating the statistics: \(String(describing: error?.localizedDescription)) ***")
+            }
+
+            stepsCollection.enumerateStatistics(from: startDate, to: now, with: { (statistics, stop) in
+                if let quantity = statistics.sumQuantity() {
+                    let date = statistics.startDate
+                    let steps = quantity.doubleValue(for: HKUnit.count())
+                    print("\(date.description(with: .current)) : steps = \(Int(steps))")
+                }
+            })
+            
+            completion(true, stepsCollection, error)
+        }
+        
+        HKHealthStore().execute(collectionQuery)
+    }
 }
-
-
-
-
-
-
