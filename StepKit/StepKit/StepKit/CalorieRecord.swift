556f21648539ad599a5a5370d78df144a9d2cf0c
Antony_Air <antony1380@gmail.com>
2018-10-20 12:18:16 +0900

    #11 Add three data types

---
+++ StepKit/StepKit/StepKit/CalorieRecord.swift
@@ -0,0 +1,15 @@
+//
+//  CalorieRecord.swift
+//  StepKit
+//
+//  Created by Antony on 2018/10/20.
+//  Copyright © 2018 CONTI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct CalorieRecord {
+    var calorie: Int?
+    var startDate: Date?
+    var endDate: Date?
+}
---
+++ StepKit/StepKit/StepKit/DistanceRecord.swift
@@ -0,0 +1,15 @@
+//
+//  DistanceRecord.swift
+//  StepKit
+//
+//  Created by Antony on 2018/10/20.
+//  Copyright © 2018 CONTI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct DistanceRecord {
+    var distance: Double?
+    var startDate: Date?
+    var endDate: Date?
+}
